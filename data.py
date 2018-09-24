1cf812513933377c8565d4fca393bc332d45ef9a
RonMilman <43000827+RonMilman@users.noreply.github.com>
2018-09-24 18:15:28 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,39 +1,14 @@
 import os
-
+import pickle
 
 def load(player):
-    data = {}
+    data = {} # hm?
     filename = get_full_path(player)
 
     try:
         if os.path.exists(filename):
-            num_lines = sum(1 for line in open(filename))
-            with open(filename) as fin:  # fin = file input
-                name = fin.readline().rstrip()
-                weapon = fin.readline().rstrip()
-                quest = fin.readline().rstrip()
-                health = fin.readline().rstrip()
-                max_health = fin.readline().rstrip()
-                defence = fin.readline().rstrip()
-                xp = fin.readline().rstrip()
-                completed = fin.readline().rstrip()
-                level = fin.readline().rstrip()
-                money = fin.readline().rstrip()
-                placeholder = fin.readline().rstrip()  # to skip the "INV" - DO NOT REMOVE
-                inventory = {}
-                for i in range(num_lines):
-                    item = fin.readline().rstrip()
-                    count = fin.readline().rstrip()
-                    if item == "" or count == "":
-                        break
-                    inventory[item] = int(count)
-
-        loaded = Player(name, weapon, quest, health, max_health, defence)
-        loaded.xp = int(xp)
-        loaded.completed = int(completed)
-        loaded.level = int(level)
-        loaded.money = int(money)
-        loaded.inventory = inventory
+            with open(filename, 'rb') as fin:  # fin = file input
+                loaded = pickle.load(fin)
         return loaded
     except TypeError:
         print("File failed to load! Threw a TypeError")
@@ -68,21 +43,8 @@ def save(player):
     # print(player)
     # print("..... saving: {}".format(player.name))
 
-    with open(filename, "w") as fout:
-        fout.write("{}\n".format(player.name))
-        fout.write("{}\n".format(player.weapon))
-        fout.write("{}\n".format(player.quest))
-        fout.write("{}\n".format(player.health))
-        fout.write("{}\n".format(player.max_health))
-        fout.write("{}\n".format(player.defence))
-        fout.write("{}\n".format(player.completed))
-        fout.write("{}\n".format(player.xp))
-        fout.write("{}\n".format(player.level))
-        fout.write("{}\n".format(player.money))
-        fout.write("INV\n")
-        for item in player.inventory:
-            fout.write(item + "\n")
-            fout.write(str(player.inventory[item]) + "\n")
+    with open(filename, "wb") as fout:
+        pickle.dump(player, fout
 
     print("Saved!")
 
@@ -92,10 +54,10 @@ def get_full_path(name):
     This method takes a string "name" and returns the named file's filepath.
 
     :param name: The name of the account file
-    :return: The full path of a .txt file
+    :return: The full path of a .pickle file
     """
-    return os.path.abspath(os.path.join('.', 'saves', name + '.sav'))
-    # takes the ".", the "accounts", and the "name.txt" to combine into an OS specific path  eg .\accounts\accounts.txt
+    return os.path.abspath(os.path.join('.', 'saves', name + '.pickle'))
+    # takes the ".", the "accounts", and the "name.pickle" to combine into an OS specific path  eg .\accounts\accounts.pickle
 
 
 def get_full_version(name):
